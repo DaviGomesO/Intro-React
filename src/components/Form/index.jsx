a966bacb2ea8e0680c1fc36572acd4f1575771f2
DaviGomesO <84361019+DaviGomesO@users.noreply.github.com>
2022-12-01 00:26:35 -0300

    Aula 2

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import React from 'react';
+import './style.css';
+
+function Form() {
+  const [nome, setNome] = useState('');
+  const [qtd_estoque, setQtdEstoque] = useState(0);
+  const [preco, setPreco] = useState(0);
+
+  //preventDefault evita a perca dos dados passados por formuláio
+  function enviarFormulario(e) {
+    e.preventDefault();
+
+    const dados = {
+        nome, 
+        qtd_estoque,
+        preco
+    };
+
+    console.log(dados);
+  }
+
+  return (
+    <form onSubmit={enviarFormulario}>
+      <h1>Cadastro de produtos</h1>
+
+      <div className="form-row">
+        <label htmlFor="nome">Nome</label>
+        <input
+          type="text"
+          name="nome"
+          id="nome"
+          onChange={(e) => setNome(e.target.value)}
+          value={nome}
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="qtd_estoque">Quantidade em estoque</label>
+        <input
+          type="number"
+          name="qtd_estoque"
+          id="qtd_estoque"
+          onChange={(e) => setQtdEstoque(e.target.value)}
+          value={qtd_estoque}
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="preco">Preço</label>
+        <input
+          type="text"
+          name="preco"
+          id="preco"
+          onChange={(e) => setPreco(e.target.value)}
+          value={preco}
+        />
+        <button type="submit">Enviar</button>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,12 +7,5 @@ import Counter from './Counter';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-
-    <Hello name="Davi">
-      {' '}
-      Seja bem-vindo(a) a disciplina de Desenvolvimento Web!{' '}
-    </Hello>
-
-    <Counter />
   </React.StrictMode>
 );
